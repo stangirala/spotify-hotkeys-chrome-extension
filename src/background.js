0e3cb6c5feacf26ddd8efe3df5b7d1bcf717c6e7
Sarma Tangirala <sarma.tangirala@tubemogul.com>
2016-11-26 14:26:49 -0800

    Remove default search, rename var

--- src/background.js
+++ src/background.js
@@ -1,15 +1,10 @@
 function onCommand(command) {
   chrome.tabs.query({url: 'https://play.spotify.com/*'}, function(tabs) {
 
-    // Open a spotify tab if one does not exist yet.
-    if (tabs.length === 0) {
-      chrome.tabs.create({url: 'https://play.spotify.com'});
-    }
-
-    var code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+    var commandsKey = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
     // Apply command on all spotify tabs.
     for (var tab of tabs) {
-      chrome.tabs.executeScript(tab.id, {code: code});
+      chrome.tabs.executeScript(tab.id, {code: commandsKey});
     }
 
     // Unload background page as soon as we're done.
